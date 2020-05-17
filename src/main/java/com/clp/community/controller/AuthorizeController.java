f5113924e7985f3cc46bf3deea33ce70e954ebcb
clp0745 <1171132498@qq.com>
2020-05-17 15:30:23 +0800

    add check cookies

--- src/main/java/com/clp/community/controller/AuthorizeController.java
+++ src/main/java/com/clp/community/controller/AuthorizeController.java
@@ -5,13 +5,16 @@ import com.clp.community.dto.GithubUser;
 import com.clp.community.mapper.UserMapper;
 import com.clp.community.mode.User;
 import com.clp.community.provider.GithubProvider;
+import com.sun.deploy.net.HttpResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.UUID;
 
 @Controller
@@ -33,7 +36,7 @@ public class AuthorizeController {
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
                            @RequestParam(name = "state") String state,
-                           HttpServletRequest request){
+                           HttpServletResponse response){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(clientId);
         accessTokenDTO.setClient_secret(clientSecret);
@@ -45,14 +48,16 @@ public class AuthorizeController {
 //        System.out.println(user.getName());
         if (githubUser != null){
             User user = new User();
-            user.setToken(UUID.randomUUID().toString());
+            String token = UUID.randomUUID().toString();
+            user.setToken(token);
             user.setName(githubUser.getName());
             user.setAccountId(String.valueOf(githubUser.getId()));
             user.setGmtCreate(System.currentTimeMillis());
             user.setGmtModified(user.getGmtCreate());
             userMapper.insert(user);
+            response.addCookie(new Cookie("token",token));
             //登录成功，写cookie和session
-            request.getSession().setAttribute("user",user);
+//            request.getSession().setAttribute("user",user);
             return "redirect:/";
         }else {
             //登录失败
